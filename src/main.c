682804d601f50a86bdbf4ab130b027def903ded1
Jared Bruni <jaredbruni@gmail.com>
2026-02-26 07:03:11 -0800

    inital tests

--- src/main.c
+++ src/main.c
@@ -4,13 +4,11 @@ void c_print(const char str[static 1]) {
 	printf("%s\n", str);
 }
 
-
 int main() {
-
+	puts("Hello, World!\n");
 	char buffer[256];
 	if(fgets(buffer, 256, stdin)) {
 		printf("value: %s\n", buffer);
 	}
-
 	return 0;
 }
