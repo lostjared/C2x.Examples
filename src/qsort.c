eb982922fed678be791df124db0e5d1990fa4671
Jared Bruni <jaredbruni@gmail.com>
2026-02-26 12:03:49 -0800

    updated stdlib qsort

---
+++ src/qsort.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int compare_value(const void *a, const void *b) {
+	return (*(int*)a) > (*(int*)b);
+}
+
+
+int main() {
+	static constexpr int ARR_SIZE = 6;
+	int array[ARR_SIZE] = {
+		[0] = 1,
+		[1] = 5,
+		[2] = 3,
+		[3] = 0,
+		[4] = -1,
+		[5] = 7
+	};
+	qsort(&array[0], ARR_SIZE-1, sizeof(int), compare_value);
+	for(unsigned int i = 0; i < ARR_SIZE; ++i) {
+		printf("%d: %d\n", i, array[i]);
+	}
+	return 0;
+}
