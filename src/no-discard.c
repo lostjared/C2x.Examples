9ca631bae68acf56a1b25ce33cbf3d567b0e564f
Jared Bruni <jaredbruni@gmail.com>
2026-03-02 06:35:45 -0800

    added nodiscard

---
+++ src/no-discard.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+[[nodiscard]] char *f_getline(char buffer[static 1], size_t bytes, FILE *fptr) {
+	char *r = fgets(buffer, bytes, fptr);
+	if(r) {
+		auto pos = strchr(r, '\n');
+		if(pos) {
+			*pos = '\0';
+			return r;
+		} 		
+	}
+	return nullptr;
+}
+
+int main(void) {
+	static constexpr ssize_t bytes = 1024 * 4;
+	char buffer[bytes];
+	printf("Enter text: ");
+	if(f_getline(buffer,  bytes-1, stdin)) {
+		printf("%s\n", buffer);
+	}
+	// must capture return value
+	// f_getline(buffer, bytes -1 , stdin);
+	return EXIT_SUCCESS;
+}
+
+
