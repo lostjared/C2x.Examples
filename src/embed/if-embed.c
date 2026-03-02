d9a03197d15ba5476dfeeb3a290ee5884e1645c7
Jared Bruni <jaredbruni@gmail.com>
2026-03-02 06:20:27 -0800

    __has_embed test

---
+++ src/embed/if-embed.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+unsigned char code[] = {
+#if defined(__has_embed) && __has_embed("if-embed.c")
+#embed "if-embed.c"
+#else
+#error "embed directive is missing"
+#endif
+};
+
+void echo_text(const unsigned char * const text, size_t s, FILE *fptr) {
+	for(size_t i = 0; i < s && text[i] != 0; ++i) {
+		fputc(text[i], fptr);
+	}
+}
+
+
+int main(void) {
+	echo_text(code, sizeof(code), stdout);
+	return EXIT_SUCCESS;
+}
