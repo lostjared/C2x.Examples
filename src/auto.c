4347f7517d64e20605fff65170a7b048e20a260e
Jared Bruni <jaredbruni@gmail.com>
2026-03-01 16:43:01 -0800

    added test for auto

---
+++ src/auto.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(int argc, char **argv) {
+	auto len = strlen("Test 1 2 3");
+	printf("len is: %d bytes\n", sizeof(len));
+	const auto x = argv[0];
+	printf("argv[0] = %s\n", x);
+	return EXIT_SUCCESS;
+}
+
