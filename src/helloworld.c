e5ebaaa24630331efc07e5d6020e4d029cf899ae
Jared Bruni <jaredbruni@gmail.com>
2026-03-02 15:31:50 -0800

    Hello, World!

---
+++ src/helloworld.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[static argc+1]) {
+	puts("Hello, World!");
+	for(int i = 0; i < argc; ++i) {
+		printf("Argument (%d): %s\n", i, argv[i]);
+	}
+	return EXIT_SUCCESS;
+}
+
