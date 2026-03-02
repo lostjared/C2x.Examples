f7050781175e05bfb1baf90fe42838286855bcea
Jared Bruni <jaredbruni@gmail.com>
2026-03-01 16:58:56 -0800

    auto test updated

--- src/auto.c
+++ src/auto.c
@@ -7,6 +7,8 @@ int main(int argc, char **argv) {
 	printf("len is: %d bytes\n", sizeof(len));
 	const auto x = argv[0];
 	printf("argv[0] = %s\n", x);
+	auto value = strtoull("0x1234",nullptr, 16);
+	printf("%ld : %d bytes\n", value, sizeof(value));
 	return EXIT_SUCCESS;
 }
 
