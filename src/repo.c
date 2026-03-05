f5a3c01b00aa0025d325ab9d43d98f4d94d2f827
Jared Bruni <jaredbruni@gmail.com>
2026-03-04 19:22:22 -0800

    unsequenced vs reproducible

--- src/repo.c
+++ src/repo.c
@@ -2,7 +2,8 @@
 #include<stdlib.h>
 #include<assert.h>
 
-int by2(int x, int y) [[reproducible]];
+int by2(int x, int y) [[reproducible]]; // does not read globals
+int by2_seq(int x, int y) [[unsequenced]]; // so unsequenced could be used
 
 int main(void) {
 	int x = 2;
@@ -11,10 +12,18 @@ int main(void) {
 	int value2 = by2(x, y);
 	assert(value1 == value2);
 	printf("value %d == %d\n", value1, value2);
+	value1 = by2_seq(x, y);
+	value2 = by2_seq(x, y);
+	assert(value1 == value2);
+	printf("value %d == %d\n", value1, value2);
 	return 0;
 }
 
 int by2(int x, int y) [[reproducible]] {
+	return x * y;
+}
+
+int by2_seq(int x, int y) [[unsequenced]] {
 	return x * y;
 }
 
