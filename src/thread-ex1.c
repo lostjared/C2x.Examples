bd8327703f0f6d5bc3d7219dba8b32edf1c5cec2
Jared Bruni <jaredbruni@gmail.com>
2026-03-17 17:15:42 -0700

    threads.h example

--- src/thread-ex1.c
+++ src/thread-ex1.c
@@ -3,19 +3,34 @@
 #include<stddef.h>
 #include<threads.h>
 
+mtx_t mut;
+
 int thread_1(void *) {
 	for(int i = 0; i < 100; ++i) {
-		printf("Hello World; %d\n", i);
+		mtx_lock(&mut);
+		printf("Hello from thread1, ; %d\n", i);
+		mtx_unlock(&mut);
+	}
+	return 0;
+}
+
+int thread_2(void *) {
+	for(int i = 0; i < 100; ++i) {
+		mtx_lock(&mut);
+		printf("Hello form thread2, : %d\n", i);
+		mtx_unlock(&mut);
 	}
 	return 0;
 }
 
 int main(void) {
-
-	thrd_t id;
-	thrd_create(&id, thread_1, nullptr);
-	thrd_join(id, nullptr);
-
+ 	mtx_init(&mut, mtx_plain);
+	thrd_t id1, id2;
+	thrd_create(&id1, thread_1, nullptr);
+	thrd_create(&id2, thread_2, nullptr);
+	thrd_join(id1, nullptr);
+	thrd_join(id2, nullptr);
+	mtx_destroy(&mut);
 	return EXIT_SUCCESS;
 }
 
