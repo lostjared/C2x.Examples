24fb1cda19a5b48533ff3cf7f69219ce34a0f516
Jared Bruni <jaredbruni@gmail.com>
2026-03-17 17:27:29 -0700

    improved notes

--- src/thread-ex1.c
+++ src/thread-ex1.c
@@ -5,29 +5,26 @@
 
 mtx_t mut;
 
-int thread_1(void *) {
+int thread_1(void *ptr) {
+	const char *name = (const char *)ptr;
 	for(int i = 0; i < 100; ++i) {
-		mtx_lock(&mut);
-		printf("Hello from thread1, ; %d\n", i);
-		mtx_unlock(&mut);
+		if(mtx_lock(&mut) == thrd_success) {
+			printf("Hello from %s, ; %d\n", name, i);
+			mtx_unlock(&mut);
+		}
 	}
 	return 0;
 }
 
-int thread_2(void *) {
-	for(int i = 0; i < 100; ++i) {
-		mtx_lock(&mut);
-		printf("Hello form thread2, : %d\n", i);
-		mtx_unlock(&mut);
-	}
-	return 0;
-}
 
 int main(void) {
- 	mtx_init(&mut, mtx_plain);
+ 	if(mtx_init(&mut, mtx_plain) != thrd_success) {
+		fprintf(stderr, "Error initalizing mutex\n");
+		return EXIT_FAILURE;
+	}
 	thrd_t id1, id2;
-	thrd_create(&id1, thread_1, nullptr);
-	thrd_create(&id2, thread_2, nullptr);
+	thrd_create(&id1, thread_1, "Thread 1");
+	thrd_create(&id2, thread_1, "Thread 2");
 	thrd_join(id1, nullptr);
 	thrd_join(id2, nullptr);
 	mtx_destroy(&mut);
