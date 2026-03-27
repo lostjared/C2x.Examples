9060a36f0e519a88094b1468f679215abba1bcce
Jared Bruni <jaredbruni@gmail.com>
2026-03-26 21:40:12 -0700

    ran clang-format

--- src/hashtable/open_address/oa_hashtable.h
+++ src/hashtable/open_address/oa_hashtable.h
@@ -1,11 +1,11 @@
 #ifndef HASH_TABLE_H
 #define HASH_TABLE_H
 
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 
 enum SlotState {
     SLOT_EMPTY = 0,
