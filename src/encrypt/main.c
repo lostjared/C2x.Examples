2527069860456d591151c6a49b6dd9716e4356ae
Jared Bruni <jaredbruni@gmail.com>
2026-04-08 08:35:32 -0700

    fix

--- src/encrypt/main.c
+++ src/encrypt/main.c
@@ -2,6 +2,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stddef.h>
+#include <stdint.h>
+static uint64_t fnv1a64_bytes(const void *data, size_t n, uint64_t h) [[unsequenced]] {
+    const uint8_t *p = (const uint8_t *)data;
+    for (size_t i = 0; i < n; ++i) {
+        h ^= p[i];
+        h *= 1099511628211ull;
+    }
+    return h;
+}
+
+static uint64_t fnv1a64_file(const char filepath[static 1]) {
+    FILE *f = fopen(filepath, "rb");
+    if (!f)
+        return 0;
+    uint64_t h = 1469598103934665603ull;
+    static constexpr size_t BUF_SIZE = 16384;
+    char buf[BUF_SIZE];
+    size_t bytes = 0;
+
+    while ((bytes = fread(buf, sizeof(char), BUF_SIZE, f)) != 0) {
+        h = fnv1a64_bytes(buf, bytes, h);
+    }
+    fclose(f);
+    return h;
+}
+
 int main(int argc, char **argv) {
 
     if (argc != 5) {
@@ -33,22 +60,48 @@ int main(int argc, char **argv) {
     unsigned char output[8] = {};
     size_t bytes = 0;
     size_t total = 0;
-    while ((bytes = fread(buffer, sizeof(char), 8, fptr)) > 0) {
-        if (mode == 1) {
-            des_encipher(buffer, output, (unsigned char *)argv[4]);
-            fwrite(output, sizeof(unsigned char), bytes, optr);
-        } else {
-            des_decipher(buffer, output, (unsigned char *)argv[4]);
-            fwrite(output, sizeof(unsigned char), bytes, optr);
-        }
-        total += bytes;
-    }
     if (mode == 1) {
-        printf("Encrypted: %zu bytes.\n", total);
+        while ((bytes = fread(buffer, 1, 8, fptr)) > 0) {
+            if (bytes < 8) {
+                unsigned char pad_val = 8 - (unsigned char)bytes;
+                memset(buffer + bytes, pad_val, pad_val);
+           }
+            des_encipher(buffer, output, (unsigned char *)argv[4]);
+            fwrite(output, 1, 8, optr);
+            total += 8;
+        }
+        if (ftell(fptr) % 8 == 0) {
+            memset(buffer, 8, 8);
+            des_encipher(buffer, output, (unsigned char *)argv[4]);
+            fwrite(output, 1, 8, optr);
+            total += 8;
+        }
     } else {
-        printf("Decrypted: %zu bytes.\n", total);
+        fseek(fptr, 0, SEEK_END);
+        auto file_size = ftell(fptr);
+        rewind(fptr);
+        while ((bytes = fread(buffer, 1, 8, fptr)) == 8) {
+            des_decipher(buffer, output, (unsigned char *)argv[4]);
+            if (ftell(fptr) == file_size) {
+                unsigned char pad_val = output[7];
+                if (pad_val > 0 && pad_val <= 8) {
+                    fwrite(output, 1, 8 - pad_val, optr);
+                    total += (8 - pad_val);
+                    break;
+                }
+            }
+            fwrite(output, 1, 8, optr);
+            total += 8;
+        }
     }
     fclose(fptr);
     fclose(optr);
+    if (mode == 1) {
+        printf("Encrypted: %zu bytes.\n", total);
+	printf("Original File Hash: %lx\n", fnv1a64_file(argv[2]));
+    } else {
+        printf("Decrypted: %zu bytes.\n", total);
+	printf("Decrypted file Hash: %lx\n", fnv1a64_file(argv[3]));
+    }
     return EXIT_SUCCESS;
 }
