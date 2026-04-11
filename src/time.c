f64a15edc9590d9e9a90f942ac98f8cbe6be98d8
Jared Bruni <jaredbruni@gmail.com>
2026-04-10 21:38:15 -0700

    struct tm

--- src/time.c
+++ src/time.c
@@ -6,6 +6,8 @@ int main(void) {
 	time_t t = time(nullptr);
 	char *value = ctime(&t);
 	printf("%s", value);
+	struct tm *t_val = localtime(&t);
+	printf("Hours: %d Minutes: %d Seconds: %d\n", t_val->tm_hour, t_val->tm_min, t_val->tm_sec);
 	return 0;
 }
 
