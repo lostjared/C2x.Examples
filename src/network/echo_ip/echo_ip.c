efec4663ebd73e593f3e293ff83efd09baedf42d
Jared Bruni <jaredbruni@gmail.com>
2026-05-11 03:45:46 -0700

    updated

--- src/network/echo_ip/echo_ip.c
+++ src/network/echo_ip/echo_ip.c
@@ -7,7 +7,7 @@
 #include <string.h>
 #include <sys/types.h>
 
-int main(void) {
+void print_ip_address() {
     struct ifaddrs *interfaces = nullptr;
     struct ifaddrs *temp_addr = nullptr;
     int pass = getifaddrs(&interfaces);
@@ -26,8 +26,42 @@ int main(void) {
         }
     } else {
         fprintf(stderr, "address lookup failed: %s\n", strerror(errno));
-        return EXIT_FAILURE;
+        exit(EXIT_FAILURE);
     }
     freeifaddrs(interfaces);
+}
+
+void echo_ip(const char *device) {
+    struct ifaddrs *interfaces = nullptr;
+    struct ifaddrs *temp_addr = nullptr;
+    int pass = getifaddrs(&interfaces);
+    if (pass == 0) {
+        temp_addr = interfaces;
+        while (temp_addr != nullptr) {
+            if (temp_addr->ifa_addr != nullptr) {
+                if (temp_addr->ifa_addr->sa_family == AF_INET) {
+                    void *temp_ptr = &((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr;
+                    char address[INET_ADDRSTRLEN];
+                    inet_ntop(AF_INET, temp_ptr, address, INET_ADDRSTRLEN);
+                    if (strcmp(temp_addr->ifa_name, device) == 0)
+                        printf("interface: %-8s IP Address: %s\n", temp_addr->ifa_name, address);
+                }
+            }
+            temp_addr = temp_addr->ifa_next;
+        }
+    } else {
+        fprintf(stderr, "address lookup failed: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    freeifaddrs(interfaces);
+}
+
+int main(int argc, char *argv[argc + 1]) {
+
+    if (argc == 1)
+        print_ip_address();
+    else if (argc == 2)
+        echo_ip(argv[1]);
+
     return EXIT_SUCCESS;
 }
