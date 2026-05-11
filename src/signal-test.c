ba6e6f9f179678ce08ad86437410518a65945e80
Jared Bruni <jaredbruni@gmail.com>
2026-05-11 00:20:13 -0700

    signal

--- src/signal-test.c
+++ src/signal-test.c
@@ -1,29 +1,26 @@
-#include<signal.h>
-#include<unistd.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 void signal_handler(int sig [[maybe_unused]]) {
-	puts("\nSignal SIGINT caught.");
-	exit(EXIT_SUCCESS);
+    puts("\nSignal SIGINT caught.");
+    exit(EXIT_SUCCESS);
 }
 
 int main(void) {
 
-	if(signal(SIGINT, signal_handler) == SIG_ERR) {
-		fprintf(stderr, "Error initalizing signal.\n");
-		return EXIT_FAILURE;
-	}
+    if (signal(SIGINT, signal_handler) == SIG_ERR) {
+        fprintf(stderr, "Error initalizing signal.\n");
+        return EXIT_FAILURE;
+    }
 
-	int index = 0;
-	while(1) {
-		++index;
-		sleep(3);
-		printf("Count index: %d\n", index);
-	}
-	return 0;
+    int index = 0;
+    while (1) {
+        ++index;
+        sleep(3);
+        printf("Count index: %d\n", index);
+    }
+    return 0;
 }
-
-
-
