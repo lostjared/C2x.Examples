c31764ed98098774773d6ee5f6d828175f5a1165
Jared Bruni <jaredbruni@gmail.com>
2026-05-11 03:52:40 -0700

    dry

--- src/network/echo_ip/echo_ip.c
+++ src/network/echo_ip/echo_ip.c
@@ -7,30 +7,6 @@
 #include <string.h>
 #include <sys/types.h>
 
-void print_ip_address() {
-    struct ifaddrs *interfaces = nullptr;
-    struct ifaddrs *temp_addr = nullptr;
-    int pass = getifaddrs(&interfaces);
-    if (pass == 0) {
-        temp_addr = interfaces;
-        while (temp_addr != nullptr) {
-            if (temp_addr->ifa_addr != nullptr) {
-                if (temp_addr->ifa_addr->sa_family == AF_INET) {
-                    void *temp_ptr = &((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr;
-                    char address[INET_ADDRSTRLEN];
-                    inet_ntop(AF_INET, temp_ptr, address, INET_ADDRSTRLEN);
-                    printf("interface: %-8s IP Address: %s\n", temp_addr->ifa_name, address);
-                }
-            }
-            temp_addr = temp_addr->ifa_next;
-        }
-    } else {
-        fprintf(stderr, "address lookup failed: %s\n", strerror(errno));
-        exit(EXIT_FAILURE);
-    }
-    freeifaddrs(interfaces);
-}
-
 void echo_ip(const char *device) {
     struct ifaddrs *interfaces = nullptr;
     struct ifaddrs *temp_addr = nullptr;
@@ -43,7 +19,7 @@ void echo_ip(const char *device) {
                     void *temp_ptr = &((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr;
                     char address[INET_ADDRSTRLEN];
                     inet_ntop(AF_INET, temp_ptr, address, INET_ADDRSTRLEN);
-                    if (strcmp(temp_addr->ifa_name, device) == 0)
+                    if (device == nullptr || strcmp(temp_addr->ifa_name, device) == 0)
                         printf("interface: %-8s IP Address: %s\n", temp_addr->ifa_name, address);
                 }
             }
@@ -59,7 +35,7 @@ void echo_ip(const char *device) {
 int main(int argc, char *argv[argc + 1]) {
 
     if (argc == 1)
-        print_ip_address();
+        echo_ip(nullptr);
     else if (argc == 2)
         echo_ip(argv[1]);
 
