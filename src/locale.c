c7b2ad45f98bb1820870615cc7b5eeea3956b0ad
Jared Bruni <jaredbruni@gmail.com>
2026-05-16 22:17:20 -0700

    locale test

---
+++ src/locale.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<locale.h>
+
+
+int main(void) {
+	if(setlocale(LC_ALL, "") == nullptr) {
+		fprintf(stderr, "Error failed to set locale.\n");
+	}
+	time_t t = time(nullptr);
+	struct tm *i = localtime(&t);
+	if(i == nullptr) {
+		fprintf(stderr, "Error: Failed to get local time.\n");
+		return EXIT_FAILURE;
+	}
+
+	char buffer[256];
+	if(strftime(buffer, sizeof(buffer), "%c", i) == 0) {
+		fprintf(stderr, "Error: strftime failed.\n");
+		return EXIT_FAILURE;
+	}
+	printf("Current time: %s\n", buffer);
+	return 0;
+}
+
