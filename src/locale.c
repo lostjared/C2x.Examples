6e0ef54b59588abd04768574660d3922341e9d42
Jared Bruni <jaredbruni@gmail.com>
2026-05-16 22:18:15 -0700

    formatting

--- src/locale.c
+++ src/locale.c
@@ -1,26 +1,24 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-#include<locale.h>
-
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(void) {
-	if(setlocale(LC_ALL, "") == nullptr) {
-		fprintf(stderr, "Error failed to set locale.\n");
-	}
-	time_t t = time(nullptr);
-	struct tm *i = localtime(&t);
-	if(i == nullptr) {
-		fprintf(stderr, "Error: Failed to get local time.\n");
-		return EXIT_FAILURE;
-	}
+    if (setlocale(LC_ALL, "") == nullptr) {
+        fprintf(stderr, "Error failed to set locale.\n");
+    }
+    time_t t = time(nullptr);
+    struct tm *i = localtime(&t);
+    if (i == nullptr) {
+        fprintf(stderr, "Error: Failed to get local time.\n");
+        return EXIT_FAILURE;
+    }
 
-	char buffer[256];
-	if(strftime(buffer, sizeof(buffer), "%c", i) == 0) {
-		fprintf(stderr, "Error: strftime failed.\n");
-		return EXIT_FAILURE;
-	}
-	printf("Current time: %s\n", buffer);
-	return 0;
+    char buffer[256];
+    if (strftime(buffer, sizeof(buffer), "%c", i) == 0) {
+        fprintf(stderr, "Error: strftime failed.\n");
+        return EXIT_FAILURE;
+    }
+    printf("Current time: %s\n", buffer);
+    return 0;
 }
-
