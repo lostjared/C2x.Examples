186a1690e72a3e5afd72e601e15c037984f86e70
Jared Bruni <jaredbruni@gmail.com>
2026-05-17 21:50:54 -0700

    list dir

---
+++ src/dir.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<dirent.h>
+#include<string.h>
+#include<errno.h>
+
+int main(int argc, char *argv[argc+1]) {
+	DIR *dir = (argc <= 1) ? opendir(".") : opendir(argv[1]);
+	if(dir == nullptr) {
+		fprintf(stderr, "Error reading from dir: %s\n", strerror(errno));
+		return EXIT_FAILURE;
+	}
+	struct dirent *d;
+	while((d = readdir(dir)) != nullptr) {
+		if(strcmp(d->d_name, "..") == 0 || strcmp(d->d_name, ".") == 0)
+			continue;
+		if(d->d_type == DT_DIR) 
+			printf("[%s]\n", d->d_name);
+		else
+			printf("%s\n", d->d_name);
+	}
+	closedir(dir);
+	return EXIT_SUCCESS;
+}
+
