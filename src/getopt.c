9a4faaf64769b1c34ff29a13232b4b7c300acf7f
Jared Bruni <jaredbruni@gmail.com>
2026-05-21 06:32:35 -0700

    getopt

--- src/getopt.c
+++ src/getopt.c
@@ -1,27 +1,25 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
-	if(argc > 1) {
-		int opt;
-		while((opt = getopt(argc, argv, "vhi:")) != -1) {
-			switch(opt) {
-			case 'v':
-				printf("Version 1.0\n");
-				break;
-			case 'h':
-				printf("getopt example\n");
-				break;
-			case 'i':
-				printf("arg value: %s\n", optarg);
-				break;
-			}
-		}
-	} else {
-		printf("%s:\nv\nh\ni <value>\n", argv[0]);
-	}
-	return EXIT_SUCCESS;
+    if (argc > 1) {
+        int opt;
+        while ((opt = getopt(argc, argv, "vhi:")) != -1) {
+            switch (opt) {
+            case 'v':
+                printf("Version 1.0\n");
+                break;
+            case 'h':
+                printf("getopt example\n");
+                break;
+            case 'i':
+                printf("arg value: %s\n", optarg);
+                break;
+            }
+        }
+    } else {
+        printf("%s:\nv\nh\ni <value>\n", argv[0]);
+    }
+    return EXIT_SUCCESS;
 }
-
