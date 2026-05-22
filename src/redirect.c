657f8ddefc87000f2ec0970990b32a954741cae4
Jared Bruni <jaredbruni@gmail.com>
2026-05-22 00:29:34 -0700

    redirect

---
+++ src/redirect.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[argc+1]) {
+	if(argc != 2) {
+		fprintf(stderr, "Error argument not provied. Use:\n%s <filename>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+	freopen(argv[1], "w", stdout);
+	printf("Redirected..\n");
+	fclose(stdout);
+	return EXIT_SUCCESS;
+}
+
