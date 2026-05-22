fd880237ee84545114952992b0fd8cfac63fc76d
Jared Bruni <jaredbruni@gmail.com>
2026-05-21 17:11:03 -0700

    file stat

--- src/file_stat.c
+++ src/file_stat.c
@@ -1,40 +1,40 @@
-#include<sys/stat.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<errno.h>
-#include<string.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
 
 int main(int argc, char **argv) {
-    if(argc == 2) {
+    if (argc == 2) {
         struct stat sb;
-        if(stat(argv[1], &sb) == -1) {
+        if (stat(argv[1], &sb) == -1) {
             printf("Error on stat: %s\n", strerror(errno));
             return EXIT_FAILURE;
         }
-        switch(sb.st_mode & S_IFMT) {
-            case S_IFREG:
-                printf("Regular file..\n");
-                break;
-            case S_IFDIR:
-                printf("Directory..\n");
-                break;
-            case S_IFCHR:
-                printf("Character device..\n");
-                break;
-            case S_IFBLK:
-                printf("Block Device..\n");
-                break;
-            case S_IFLNK:
-                printf("Soft link..\n");
-                break;
-            case S_IFIFO:
-                printf("FIFO or pipe..\n");
-                break;
-            case S_IFSOCK:
-                printf("Socket..\n");
-                break;
-            default:
-                printf("Unknown file type..\n");
+        switch (sb.st_mode & S_IFMT) {
+        case S_IFREG:
+            printf("Regular file..\n");
+            break;
+        case S_IFDIR:
+            printf("Directory..\n");
+            break;
+        case S_IFCHR:
+            printf("Character device..\n");
+            break;
+        case S_IFBLK:
+            printf("Block Device..\n");
+            break;
+        case S_IFLNK:
+            printf("Soft link..\n");
+            break;
+        case S_IFIFO:
+            printf("FIFO or pipe..\n");
+            break;
+        case S_IFSOCK:
+            printf("Socket..\n");
+            break;
+        default:
+            printf("Unknown file type..\n");
         }
     } else {
         printf("Requires one argument file\n");
