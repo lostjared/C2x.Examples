5fa04f8ad7ca0245448a1070be15e19b9dfb37f0
Jared Bruni <jaredbruni@gmail.com>
2026-05-21 17:00:46 -0700

    uname

---
+++ src/uname.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/utsname.h>
+#include<unistd.h>
+#include<errno.h>
+#include<string.h>
+
+int main(void) {
+	struct utsname uts;
+	if(uname(&uts) == -1) {
+		fprintf(stderr, "Error on uname: %s\n", strerror(errno));
+		return EXIT_FAILURE;
+	}
+
+	printf("Node: %s\nSystem: %s\nRelease: %s\nVersion: %s\nMachine: %s\n", uts.nodename, uts.sysname, uts.release, uts.version, uts.machine);
+	return EXIT_SUCCESS;
+}
+
