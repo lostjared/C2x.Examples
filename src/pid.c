4b4fd4f28c823d4d69f08b216af2ad5ff3c61e27
Jared Bruni <jaredbruni@gmail.com>
2026-05-23 03:37:29 -0700

    formatting

--- src/pid.c
+++ src/pid.c
@@ -1,9 +1,8 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int main(void) {
-	printf("process id: %d\n", getpid());
-	return EXIT_SUCCESS;
+    printf("process id: %d\n", getpid());
+    return EXIT_SUCCESS;
 }
-
