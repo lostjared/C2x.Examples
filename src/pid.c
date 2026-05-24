250f22aed7e342bc0e8ca0dc35f846dbc605b3b9
Jared Bruni <jaredbruni@gmail.com>
2026-05-24 01:08:49 -0700

    process information

--- src/pid.c
+++ src/pid.c
@@ -3,26 +3,37 @@
 #include <unistd.h>
 #include <string.h>
 
-int main(void) {
-    static constexpr size_t BUFFER_SIZE = 4096;
-    pid_t id = getpid();
-    printf("process id: %d\n", id);
+static constexpr size_t BUFFER_SIZE = 4096;
+
+void print_info(pid_t id, const char *file_info) {
     char file_buffer[BUFFER_SIZE];
     memset(file_buffer, 0, sizeof(file_buffer));
-    snprintf(file_buffer, BUFFER_SIZE, "/proc/%d/cmdline", id);
+    snprintf(file_buffer, BUFFER_SIZE, "/proc/%d/%s", id, file_info);
     FILE *fptr = fopen(file_buffer, "r");
     if(!fptr) {
-	    fprintf(stderr, "Error could not find process..\n");
-	    return EXIT_FAILURE;
+	    fprintf(stderr, "Error could not find process file: %s.\n", file_buffer);
+	    return;
     }
     char buffer[BUFFER_SIZE];
     size_t bytes = 0;
     memset(buffer, 0, sizeof(buffer));
-    printf("Information: \n");
-    while((bytes = fread(buffer, 1, BUFFER_SIZE, fptr)) > 0) {
-	    printf("%s\n", buffer);
+    while((bytes = fread(buffer, 1, BUFFER_SIZE - 1, fptr)) > 0) {
+	    for(size_t i = 0; i < bytes; ++i)
+		    if(buffer[i] == '\0')
+			    buffer[i] = ' ';		   
+	    printf("%s", buffer);
 	    memset(buffer, 0, sizeof(buffer));
     }
+    printf("\n");
     fclose(fptr);
-    return EXIT_SUCCESS;
+}
+
+int main(void) {
+    pid_t id = getpid();
+    printf("process id: %d\n", id);
+    print_info(id, "cmdline");
+    print_info(id, "status");
+    print_info(id, "environ");
+    print_info(id, "maps");
+   return EXIT_SUCCESS;
 }
