62a3ab1cf8f86853e3dda3aec74bd43cd4be7685
Jared Bruni <jaredbruni@gmail.com>
2026-05-26 02:13:09 -0700

    formatting

--- src/fork.c
+++ src/fork.c
@@ -1,29 +1,27 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/wait.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 int main(void) {
-	printf("Process started..\n");
-	pid_t id = fork();
-	switch(id) {
-		case -1:
-		printf("Error on fork.\n");
-		exit(EXIT_FAILURE);
-		break;
-		case 0:
-		printf("In child process..\n");
-		_exit(EXIT_SUCCESS);
-		break;
-		default:
-		printf("in parent..\n");
-		break;
-	}
-	int status = 0;
-	waitpid(id, &status, 0);
-	printf("Program complete..\n");
-	return EXIT_SUCCESS;
+    printf("Process started..\n");
+    pid_t id = fork();
+    switch (id) {
+    case -1:
+        printf("Error on fork.\n");
+        exit(EXIT_FAILURE);
+        break;
+    case 0:
+        printf("In child process..\n");
+        _exit(EXIT_SUCCESS);
+        break;
+    default:
+        printf("in parent..\n");
+        break;
+    }
+    int status = 0;
+    waitpid(id, &status, 0);
+    printf("Program complete..\n");
+    return EXIT_SUCCESS;
 }
-
