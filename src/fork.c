387117316175c5e1e773b8c04bae5396b2e7aaf1
Jared Bruni <jaredbruni@gmail.com>
2026-05-26 02:12:54 -0700

    fork test

---
+++ src/fork.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+
+
+int main(void) {
+	printf("Process started..\n");
+	pid_t id = fork();
+	switch(id) {
+		case -1:
+		printf("Error on fork.\n");
+		exit(EXIT_FAILURE);
+		break;
+		case 0:
+		printf("In child process..\n");
+		_exit(EXIT_SUCCESS);
+		break;
+		default:
+		printf("in parent..\n");
+		break;
+	}
+	int status = 0;
+	waitpid(id, &status, 0);
+	printf("Program complete..\n");
+	return EXIT_SUCCESS;
+}
+
