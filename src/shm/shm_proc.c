d309e936da4c0f91a229c7fe35c402e7cf1a2e93
Jared Bruni <jaredbruni@gmail.com>
2026-05-27 06:39:50 -0700

    shm unlink

--- src/shm/shm_proc.c
+++ src/shm/shm_proc.c
@@ -13,6 +13,7 @@ int main() {
 
 	int fd;
 	void *addr;
+	size_t shm_size;
 
 	fd = shm_open("/proc_program", O_RDONLY, 0);
 	if(fd == -1) {
@@ -25,8 +26,10 @@ int main() {
 		perror("fstat");
 		return EXIT_FAILURE;
 	}
+	
+	shm_size = (size_t)st_buf.st_size;
 
-	addr = mmap(nullptr, (size_t)st_buf.st_size, PROT_READ, MAP_SHARED, fd, 0);
+	addr = mmap(nullptr, shm_size, PROT_READ, MAP_SHARED, fd, 0);
 	if(addr == MAP_FAILED) {
 		perror("mmap");
 		return EXIT_FAILURE;
@@ -35,6 +38,15 @@ int main() {
 	close(fd);
 	struct shared_data *d = (struct shared_data *)addr;
 	printf("%d\n", d->value);
+
+	if(munmap(addr, shm_size) == -1) {
+		perror("munmap");
+	}
+
+	if(shm_unlink("/proc_program") == -1) {
+		perror("shm_unlink");
+	}
+
 	return EXIT_SUCCESS;
 }
 
