773c6c33cf7a01cfccf63784f4b57825fca74fae
Jared Bruni <jaredbruni@gmail.com>
2026-05-29 03:58:44 -0700

    read/write

--- src/pipe.c
+++ src/pipe.c
@@ -7,6 +7,42 @@
 #include <time.h>
 #include <unistd.h>
 
+ssize_t write_all(int fd, const void *buf, size_t bytes) {
+    const char *ptr = buf;
+    size_t left = bytes;
+    while (left > 0) {
+        ssize_t written = write(fd, ptr, left);
+        if (written == -1) {
+            if (errno == EINTR) {
+                continue;
+            }
+            return -1;
+        }
+        left -= (size_t)written;
+        ptr += written;
+    }
+    return (ssize_t)bytes;
+}
+
+ssize_t read_all(int fd, void *buf, size_t bytes) {
+    char *ptr = buf;
+    size_t left = bytes;
+    while (left > 0) {
+        ssize_t bytes_read = read(fd, ptr, left);
+        if (bytes_read == -1) {
+            if (errno == EINTR) {
+                continue;
+            }
+            return -1;
+        }
+        if (bytes_read == 0)
+            break;
+        left -= (size_t)bytes_read;
+        ptr += bytes_read;
+    }
+    return (ssize_t)(bytes - left);
+}
+
 int main(void) {
     int fd[2];
     if (pipe(fd) == -1) {
@@ -24,13 +60,13 @@ int main(void) {
         int message = rand() % 255;
         printf("Sending message: %d\n", message);
         close(fd[0]);
-        write(fd[1], &message, sizeof(message));
+        write_all(fd[1], &message, sizeof(message));
         close(fd[1]);
         _exit(EXIT_SUCCESS);
     } else {
         close(fd[1]);
         int message = 0;
-        ssize_t rb = read(fd[0], &message, sizeof(message));
+        ssize_t rb = read_all(fd[0], &message, sizeof(message));
         if (rb > 0) {
             printf("Child returned: %d\n", message);
             close(fd[0]);
