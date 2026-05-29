b7ffacc6f5954173756a823fa5a5062fe651eb8f
Jared Bruni <jaredbruni@gmail.com>
2026-05-29 04:04:31 -0700

    corrected

--- src/pipe.c
+++ src/pipe.c
@@ -56,7 +56,7 @@ int main(void) {
         perror("fork");
         return EXIT_FAILURE;
     } else if (id == 0) {
-        srand((unsigned int)time(0));
+        srand((unsigned int)time(nullptr));
         int message = rand() % 255;
         printf("Sending message: %d\n", message);
         close(fd[0]);
@@ -71,11 +71,11 @@ int main(void) {
         ssize_t rb = read_all(fd[0], &message, sizeof(message));
         if (rb == (ssize_t)sizeof(message)) {
             printf("Child returned: %d\n", message);
-            close(fd[0]);
         } else if (rb == -1)
             perror("read");
         else
             fprintf(stderr, "Unexpected EOF (end of file) or partial read\n");
+        close(fd[0]);
     }
     waitpid(id, 0, 0);
     return EXIT_SUCCESS;
