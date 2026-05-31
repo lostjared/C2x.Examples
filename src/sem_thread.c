1637b7b8cce211dde11274161a9442f0d50a94e5
Jared Bruni <jaredbruni@gmail.com>
2026-05-31 02:25:13 -0700

    formatting/binary semaphore

--- src/sem_thread.c
+++ src/sem_thread.c
@@ -1,39 +1,37 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<semaphore.h>
-#include<threads.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <threads.h>
+#include <unistd.h>
 
 struct data {
-	sem_t s;
-	int counter;
+    sem_t s;
+    int counter;
 };
 
 int thread_proc(void *ptr) {
-	struct data *d = ptr;
-	sem_wait(&d->s);
-	sleep(1);
-	d->counter ++;
-	printf("Thread woke up: %d\n", d->counter);	
-	sem_post(&d->s);
-	return 0;
+    struct data *d = ptr;
+    sem_wait(&d->s);
+    sleep(1);
+    d->counter++;
+    printf("Thread woke up: %d\n", d->counter);
+    sem_post(&d->s);
+    return 0;
 }
-
 
 int main(void) {
-	struct data d = {};
-	if(sem_init(&d.s, 0, 1) == -1) {
-		perror("sem_init");
-		return EXIT_FAILURE;
-	}
-	thrd_t t;
-	thrd_create(&t, thread_proc, &d);
-	sem_wait(&d.s);
-	d.counter ++;
-	printf("Main thread woke up: %d...\n", d.counter);
-	sem_post(&d.s);
-	thrd_join(t, nullptr);
-	sem_destroy(&d.s);
-	return EXIT_SUCCESS;
+    struct data d = {};
+    if (sem_init(&d.s, 0, 1) == -1) {
+        perror("sem_init");
+        return EXIT_FAILURE;
+    }
+    thrd_t t;
+    thrd_create(&t, thread_proc, &d);
+    sem_wait(&d.s);
+    d.counter++;
+    printf("Main thread woke up: %d...\n", d.counter);
+    sem_post(&d.s);
+    thrd_join(t, nullptr);
+    sem_destroy(&d.s);
+    return EXIT_SUCCESS;
 }
-
