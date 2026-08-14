30ff2613a447a52a2e4a9977311c185403a89e16
Jared Bruni <jaredbruni@gmail.com>
2026-08-14 01:00:51 -0700

    fifo_serv/cli

---
+++ src/fifo_cli.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<signal.h>
+#include<string.h>
+
+#define FIFO_PATH "/tmp/fifo_file_X"
+static constexpr size_t MESSAGE_LEN = 256;
+
+int main(int argc, char **argv) {
+	if(argc != 2) {
+		fprintf(stderr, "Error invalid arguments take one argument.\n");
+		exit(EXIT_FAILURE);
+	}
+	int fd = open(FIFO_PATH, O_WRONLY);
+	if(!fd) {
+		perror("open");
+		return EXIT_FAILURE;
+	}
+	size_t len = strlen(argv[1]);
+	if(len > MESSAGE_LEN-1) {
+		fprintf(stderr, "Message too Long.\n");
+		return EXIT_FAILURE;
+	}
+	if(write(fd, argv[1], len) != (ssize_t)len) {
+		perror("write");
+	}
+	close(fd);
+	return EXIT_SUCCESS;
+}
+
+
